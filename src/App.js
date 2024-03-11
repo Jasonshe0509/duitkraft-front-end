5f15d16b06e278081f8ceb8678605d063f680211
Jasonshe0509 <jasonshe80@gmail.com>
2024-03-11 08:39:13 +0800

    fix: modify naming

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Hallo
+          Learn about World
         </a>
       </header>
     </div>
