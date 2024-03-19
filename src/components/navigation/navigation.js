e41edb769023653108103156f4c9c8fd5127dc06
Jasonshe0509 <jasonshe80@gmail.com>
2024-03-19 09:29:07 +0800

    fix: link page in navigation

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   const [open, setOpen] = useState(true);
-  const Menus = [{ title: "Dashboard", icon: <HomeIcon className="w-6 h-6 text-fontColor" /> },
+  const Menus = [{ title: "Dashboard", icon: <HomeIcon className="w-6 h-6 text-fontColor" />, link: "/dashboard" },
   {
     title: "Education", icon: <AcademicCapIcon className="w-6 h-6 text-fontColor" />,
-    submenu: true, submenuItems: [{ title: "Course", icon: <DocumentIcon className="w-6 h-6 text-fontColor" /> }, { title: "Module", icon: <DocumentDuplicateIcon className="w-6 h-6 text-fontColor" /> }]
+    submenu: true, submenuItems: [{ title: "Course", icon: <DocumentIcon className="w-6 h-6 text-fontColor" />, link: "/dashboard" }, { title: "Module", icon: <DocumentDuplicateIcon className="w-6 h-6 text-fontColor" />, link: "/dashboard" }]
   },
-  { title: "News", icon: <NewspaperIcon className="w-6 h-6 text-fontColor" /> },
-  { title: "Market Value", icon: <ChartBarIcon className="w-6 h-6 text-fontColor" /> }];
+  { title: "News", icon: <NewspaperIcon className="w-6 h-6 text-fontColor" />, link: "/dashboard" },
+  { title: "Market Value", icon: <ChartBarIcon className="w-6 h-6 text-fontColor" />, link: "/dashboard" }];
 
 
 
@@ -24,14 +24,16 @@ const Navigation = () => {
             <div className="menuone right-0 mr-0 flex flex-col items-center justify-center h-full text-fontColor shadow-xl">></div>
           </div>
 
-          <div className={`logowithword w-12 h-12 text-fontColor p-1 ${open ? "" : "hidden"}`}></div>
-          <div className={`logoonly w-12 h-12 text-fontColor p-1 ${open && "hidden"}`}></div>
+          <Link to='/dashboard'>
+            <div className={`logowithword w-12 h-12 text-fontColor p-1 ${open ? "" : "hidden"}`}></div>
+            <div className={`logoonly w-12 h-12 text-fontColor p-1 ${open && "hidden"}`}></div>
+          </Link>
 
           <Link to='/profile'>
             <div >
               <img src={profile_image} className="w-16 h-16 m-2 rounded-full" />
             </div>
-          <div className={`username text-fontColor focus:outline-none ${!open && "hidden"}`}>username</div>
+            <div className={`username text-fontColor focus:outline-none ${!open && "hidden"}`}>username</div>
           </Link>
           <div className="flex justify-center">
             <span className={`username2 text-fontColor focus:outline-none ${!open && "hidden"}`}>lvl1</span>
@@ -41,22 +43,26 @@ const Navigation = () => {
           <ul className="pt-1 text-fontColor margin-10 center">
             {Menus.map((menu, index) => (
               <li key={index} className={`flex flex-col rounded-md p-2 cursor-pointer hover:bg-hoverr text-fontColor text-sm gap-x-2 ${Menus.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-gray-900"}`}>
-                <div className="flex items-center gap-x-2">
-                  {menu.icon}
-                  <span className={`focus:outline-none ${!open && "hidden"}`}>
-                    <p>{menu.title}</p>
-                  </span>
-                </div>
+                <Link to={menu.link}>
+                  <div className="flex items-center gap-x-2">
+                    {menu.icon}
+                    <span className={`focus:outline-none ${!open && "hidden"}`}>
+                      <p>{menu.title}</p>
+                    </span>
+                  </div>
+                </Link>
                 {menu.submenu && open && menu.submenuItems && (
                   <ul className="ml-4 list-none">
                     {menu.submenuItems.map((submenu, subIndex) => (
                       <li key={subIndex} className={`cursor-pointer rounded-md p-1 hover:bg-white text-fontColor text-sm items-center gap-x-5 mr-2 gap-x-2 ${Menus.gap ? "mt-9" : "mt-2"}`}>
-                        <div className="flex items-center gap-x-2">
-                          {submenu.icon}
-                          <span className={`focus:outline-none ${!open && "hidden"}`}>
-                            <p>{submenu.title}</p>
-                          </span>
-                        </div>
+                        <Link to={submenu.link}>
+                          <div className="flex items-center gap-x-2">
+                            {submenu.icon}
+                            <span className={`focus:outline-none ${!open && "hidden"}`}>
+                              <p>{submenu.title}</p>
+                            </span>
+                          </div>
+                        </Link>
                       </li>
                     ))}
                   </ul>
